ae8c3d6d3672e78eb5d8a13e2471623cb6be0176
Your Name <you@example.com>
2018-06-26 19:45:40 +0700

    Bai thuc hanh

---
+++ Thuchanh/Store/Store/Product.cs
@@ -0,0 +1,34 @@
+﻿namespace Store
+{
+    public class Product
+    {
+        private string productID;
+        private string name;
+        private int price;
+
+        public Product(string productId, string name, int price)
+        {
+            productID = productId;
+            this.name = name;
+            this.price = price;
+        }
+
+        public string ProductId
+        {
+            get => productID;
+            set => productID = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}
